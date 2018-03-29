94437845034b3a6cc48bdc172ea93786d42d020c
czabriskie <31149669+czabriskie@users.noreply.github.com>
2018-03-29 14:15:41 -0600

    Just tested a few things, nothing too major. I will now stop changing the master :)

--- Project_2/H1B_code.R
+++ Project_2/H1B_code.R
@@ -1,7 +1,18 @@
-library(randomForest)
-H1B1 <- read.csv('C:\\Users\\camer\\Desktop\\H1B data\\1. Master H1B Dataset.csv')
-H1B1_1 <- read.csv('C:\\Users\\camer\\Desktop\\H1B data\\File 1 - H1B Dataset.csv')
-summary(H1B1$CASE_STATUS)
-summary(H1B1_1)
+require(randomForest)
+require(dplyr)
 
+H1B <- read.csv('./Project_2/H1B_Data/1. Master H1B Dataset.csv')
+H1B_1 <- read.csv('./Project_2/H1B_Data/File 1 - H1B Dataset.csv')
+
+summary(H1B$CASE_STATUS)
+summary(H1B_1$CASE_STATUS)
+
+lapply(H1B, class)
+
+# a bunch of NA values, but I don't think we should just delete them like this
+H1B1_1 <- na.omit(H1B1_1)
+
+# Random Forest wont run even with obs with NA in them removed, since it can't handle more than 53 categories
 randomForest(CASE_STATUS ~ ., data = H1B1_1)
+
+             
